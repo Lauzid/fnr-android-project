6e3633e89ea33fe20eb748849d0bfced84ef7d50
GustavoSS01 <gustavodssilva456@gmail.com>
2024-06-07 15:07:40 -0300

    fix(show-mainactivity)

--- app/src/main/java/com/example/fnr_android_project/ui/MainActivity.kt
+++ app/src/main/java/com/example/fnr_android_project/ui/MainActivity.kt
@@ -1,28 +1,40 @@
 package com.example.fnr_android_project.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.fnr_android_project.R
 import com.example.fnr_android_project.databinding.ActivityMainBinding
+import com.example.fnr_android_project.model.User
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val USER = "user"
+    }
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.container) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-    companion object {
-        const val USER = "USER"
+
+        val user = intent.getParcelableExtra<User>(USER)
+        user?.let {
+            binding.username.text = it.name
+        }
+
+        binding.collectionsButton.setOnClickListener {
+            // Adicione aqui o código para navegar para a tela de Collections
+        }
+
+        binding.favoritesButton.setOnClickListener {
+            // Adicione aqui o código para navegar para a tela de Favorites
+        }
     }
 }
