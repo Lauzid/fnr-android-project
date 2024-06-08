595eea700a3a8e76e89f49b8d58c81b0dcb2cd11
GustavoSS01 <gustavodssilva456@gmail.com>
2024-06-08 16:26:41 -0300

    fix(mainactivity-now-working)

--- app/src/main/java/com/example/fnr_android_project/ui/MainActivity.kt
+++ app/src/main/java/com/example/fnr_android_project/ui/MainActivity.kt
@@ -1,45 +1,47 @@
 package com.example.fnr_android_project.ui
 
-import android.content.Intent
+//import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.fnr_android_project.R
 import com.example.fnr_android_project.databinding.ActivityMainBinding
+import com.example.fnr_android_project.getExtra
 import com.example.fnr_android_project.model.User
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val USER = "user"
+    }
+
     private lateinit var binding: ActivityMainBinding
-    private var user: User? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.container) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        binding.collectionsButton.setOnClickListener {
-            val intent = Intent(this, ViewCollections::class.java)
-            intent.putExtra(USER, user?.name)
-            startActivity(intent)
-        }
-
-        binding.favoritesButton.setOnClickListener {
-            val intent = Intent(this, FavoritesActivity::class.java)
-            intent.putExtra(USER, user?.name)
-            startActivity(intent)
-        }
-
+        setUpView()
+//        setUpListeners()
+    }
+    private fun setUpView(){
+        val user = intent.getExtra<User>(USER)
+        binding.username.text = user?.name
     }
 
-    companion object {
-        const val USER = "USER"
-    }
+//    private fun setUpListeners(){
+//        binding.collectionsButton.setOnClickListener {
+//            val intent = Intent(this, ViewCollectionActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        binding.favoritesButton.setOnClickListener {
+//            val intent = Intent(this, ViewCollectionActivity::class.java)
+//            startActivity(intent)
+//        }
+//    }
 }
