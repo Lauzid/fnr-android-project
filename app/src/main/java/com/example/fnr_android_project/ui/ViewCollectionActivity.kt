445d4ff12077b4430ecd507936641a075040f6e9
Lauzid <adrianlauzid@gmail.com>
2024-06-08 13:14:02 -0300

    ft(add-collections-activity): adição da activity de coleções

---
+++ app/src/main/java/com/example/fnr_android_project/ui/ViewCollectionActivity.kt
@@ -0,0 +1,73 @@
+package com.example.fnr_android_project.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fnr_android_project.R
+import com.example.fnr_android_project.databinding.ActivityViewCollectionBinding
+import com.example.fnr_android_project.network.Network
+import com.example.fnr_android_project.ui.collection_recycler_view.CollectionRecyclerViewAdapter
+import com.example.fnr_android_project.model.Set
+
+const val SET = "set"
+class ViewCollectionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityViewCollectionBinding
+    private var sets = mutableListOf<Set>()
+    private lateinit var collectionRecyclerViewAdapter: CollectionRecyclerViewAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityViewCollectionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        setUpView()
+        loadSets()
+    }
+    private fun setUpView() {
+        binding.apply {
+            collectionRecyclerViewAdapter = CollectionRecyclerViewAdapter(sets) {
+                val intent = Intent(this@ViewCollectionActivity, ActivityViewCollection::class.java)
+                intent.putExtra(SET, it)
+                startActivity(intent)
+            }
+            collectionRecyclerView.layoutManager = LinearLayoutManager(this@ViewCollectionActivity)
+            collectionRecyclerView.adapter = collectionRecyclerViewAdapter
+        }
+    }
+    private fun loadSets() {
+        Network.api.getSets().enqueue(
+            object : retrofit2.Callback<List<Set>> {
+                @SuppressLint("NotifyDataSetChanged")
+                override fun onResponse(
+                    call: retrofit2.Call<List<Set>>,
+                    response: retrofit2.Response<List<Set>>
+                ) {
+                    if (response.isSuccessful) {
+                        sets.clear()
+                        sets.addAll(response.body()!!)
+                        collectionRecyclerViewAdapter.notifyDataSetChanged()
+                    } else {
+                        Toast.makeText(
+                            this@ViewCollectionActivity,
+                            "Error loading houses",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+
+                override fun onFailure(call: retrofit2.Call<List<Set>>, t: Throwable) {
+                    t.printStackTrace()
+                }
+            })
+    }
+}
---
+++ app/src/main/java/com/example/fnr_android_project/ui/collection_recycler_view/CollectionViewHolder.kt
@@ -0,0 +1,16 @@
+package com.example.fnr_android_project.ui.collection_recycler_view
+
+import com.example.fnr_android_project.databinding.CollectionItemBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fnr_android_project.model.Set
+class CollectionViewHolder {
+    class CollectionViewHolder (private val collectionItemBinding: CollectionItemBinding) :
+        RecyclerView.ViewHolder(collectionItemBinding.root) {
+        fun bind(set: Set, onItemClicked: (Set) -> Unit) {
+            collectionItemBinding.apply {
+                root.setOnClickListener { (onItemClicked(set)) }
+                collectionName.text = set.name
+            }
+        }
+    }
+}
